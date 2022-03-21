e9075b7d041360f41ca4233b082961a0dfce6cc4
efess29 <efess29@gmail.com>
2022-03-21 22:18:47 +0300

    add task 2

---
+++ Lesson-2/Lesson2_Task2/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Task2
+{
+    /// <summary>
+    /// Представляет класс банковского счета
+    /// </summary>
+    class BankAccount
+    {
+        private static Guid _generatedAccountNumber;
+        private decimal? _accountBalance;
+        private AccountType _accountType;
+
+        /// <summary>
+        /// Представляет метод генерации номера счета
+        /// </summary>
+        /// <returns></returns>
+        public string GetGeneratedAccountNumber()
+        {
+            return _generatedAccountNumber.ToString();
+        }
+
+        /// <summary>
+        /// Представляет метод получения сгенерированного номера счета
+        /// </summary>
+        public void SetGeneratedAccountNumber()
+        {
+            _generatedAccountNumber = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Представляет метод получения баланса
+        /// </summary>
+        /// <returns></returns>
+        public decimal? GetAccountBalance()
+        {
+            return _accountBalance;
+        }
+
+        /// <summary>
+        /// Представляет метод заполнения баланса
+        /// </summary>
+        public void SetAccountBalance()
+        {
+            _accountBalance = 455532.19m;
+        }
+
+        /// <summary>
+        /// Представляет метод получения типа счета
+        /// </summary>
+        /// <returns></returns>
+        public AccountType GetAccountType()
+        {
+            return _accountType;
+        }
+
+        /// <summary>
+        /// Представляет метод заполнения типа счета
+        /// </summary>
+        public void SetAccountType()
+        {
+            _accountType = (AccountType)1;
+        }
+
+        /// <summary>
+        /// Представляет перечисление типов счетов
+        /// </summary>
+        [Flags]
+        public enum AccountType
+        {
+            Credit = 0,
+            Debit = 1
+        }
+    }
+}
---
+++ Lesson-2/Lesson2_Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lesson2_Task2.BankAccount;
+
+namespace Lesson2_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            account.SetAccountBalance();
+            account.SetAccountType();
+            account.SetGeneratedAccountNumber();
+
+            string number = account.GetGeneratedAccountNumber();
+            decimal? balance = account.GetAccountBalance();
+            AccountType type = account.GetAccountType();
+
+            Console.WriteLine("BANK ACCOUNT INFO");
+            Console.WriteLine("=====================");
+            Console.WriteLine($"Account number: {number}");
+            Console.WriteLine($"Account balance: {balance}");
+            Console.WriteLine($"Account balance: {type}");
+
+            Console.ReadLine();
+        }
+    }
+}
