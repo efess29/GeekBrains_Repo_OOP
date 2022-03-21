e7de97bc85d37981ba0f6fcd433fd5c3b075a4c5
efess29 <efess29@gmail.com>
2022-03-21 12:59:53 +0300

    add task 1

---
+++ Lesson-2/Lesson2_Task1/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lesson2_Task1
+{
+    public class BankAccount
+    {
+        private string _accountNumber;
+        private decimal? _accountBalance;
+        private AccType _accountType;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+
+            set
+            {
+                this._accountNumber = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal? AccountBalance
+        {
+            get
+            {
+                return _accountBalance;
+            }
+
+            set
+            {
+                this._accountBalance = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public AccType AccountType
+        {
+            get
+            {
+                return _accountType;
+            }
+
+            set
+            {
+                this._accountType = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Flags]
+        public enum AccType
+        {
+            Credit = 0,
+            Debit = 1
+        }
+    }
+}
