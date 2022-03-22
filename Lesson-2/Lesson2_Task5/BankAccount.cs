d03b15f1aa2591d24fb0aee4f523d971f0487fb0
efess29 <efess29@gmail.com>
2022-03-22 15:42:58 +0300

    add task 5

---
+++ Lesson-2/Lesson2_Task5/BankAccount.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Lesson2_Task5
+{
+    /// <summary>
+    /// Представляет класс банковского счета
+    /// </summary>
+    class BankAccount
+    {
+        private static Guid _accountNumber;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="BankAccount">
+        /// </summary>
+        public BankAccount(decimal? balance, AccountType type)
+        {
+            SetGeneratedAccountNumber();
+            _accountNumber = GetGeneratedAccountNumber();
+            this.AccountBalance = balance; //562310.62m;
+            this.AccountKind = type; //(AccountType)1;
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="BankAccount"> с заполненным балансом
+        /// </summary>
+        /// <param name="balance"></param>
+        public BankAccount(decimal? balance)
+        {
+            SetGeneratedAccountNumber();
+            _accountNumber = GetGeneratedAccountNumber();
+            this.AccountBalance = balance;
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="BankAccount"> с заполненным типом счета
+        /// </summary>
+        /// <param name="type"></param>
+        public BankAccount(AccountType type)
+        {
+            SetGeneratedAccountNumber();
+            _accountNumber = GetGeneratedAccountNumber();
+            this.AccountKind = type;
+        }
+
+        /// <summary>
+        /// Получает или задает номер счета
+        /// </summary>
+        public Guid AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+
+            set
+            {
+                SetGeneratedAccountNumber();
+            }
+        }
+
+        /// <summary>
+        /// Получает или задает баланс на счете
+        /// </summary>
+        public decimal? AccountBalance { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public AccountType? AccountKind { get; set; }
+
+        /// <summary>
+        /// Представляет метод генерации номера счета
+        /// </summary>
+        /// <returns></returns>
+        public Guid GetGeneratedAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        /// <summary>
+        /// Представляет метод получения сгенерированного номера счета
+        /// </summary>
+        public void SetGeneratedAccountNumber()
+        {
+            _accountNumber = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public void WithdrawMoney(decimal value)
+        {
+            try
+            {
+                if (value <= 0)
+                    throw new Exception("Unable to withdraw the specified amount. Enter another amount.");
+
+                var checkAmount = (this.AccountBalance >= value) ? true : false;
+
+                if (checkAmount)
+                {
+                    this.AccountBalance -= value;
+                    Console.WriteLine($"Operation successfull! You withdrawed: {value}.");
+                    Console.WriteLine($"Your current account balance: {this.AccountBalance}.");
+                }
+                    
+                else
+                {
+                    throw new Exception("Unable to withdraw the specified amount. Insufficient funds.");
+                }
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception information: {0}", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public void DepositMoney(decimal value)
+        {
+            try
+            {
+                if (value <= 0)
+                    throw new Exception("Unable to deposit the specified amount. Enter another amount.");
+
+                this.AccountBalance += value;
+                Console.WriteLine($"Operation successfull! You deposited: {value}.");
+                Console.WriteLine($"Your current account balance: {this.AccountBalance}.");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception information: {0}", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Представляет перечисление типов счетов
+        /// </summary>
+        [Flags]
+        public enum AccountType
+        {
+            Credit = 0,
+            Debit = 1
+        }
+    }
+}
---
+++ Lesson-2/Lesson2_Task5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using static Lesson2_Task5.BankAccount;
+
+namespace Lesson2_Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("================================");
+                Console.WriteLine("Welcome to BANK ACCOUNT MANAGER!");
+                Console.WriteLine("================================");
+                Console.WriteLine();
+
+                Console.WriteLine("Please, enter your bank account params - balance and type.");
+                Console.Write("Account balance: ");
+                var balance = Decimal.Parse(Console.ReadLine());
+                Console.Write("Account type (0 - Credit, 1 - Debit): ");
+                var type = (AccountType)Int32.Parse(Console.ReadLine());
+
+                var accountDefault = new BankAccount(balance, type);
+
+                Console.WriteLine();
+                Console.WriteLine("Your bank account info: ");
+                Console.WriteLine("================================");
+                Console.WriteLine($"Account number: {accountDefault.AccountNumber}");
+                Console.WriteLine($"Account balance: {accountDefault.AccountBalance}");
+                Console.WriteLine($"Account type: {accountDefault.AccountKind}");
+                Console.WriteLine("================================");
+
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Withdraw amount");
+                Console.WriteLine("2. Deposit amount");
+                Console.WriteLine("3. Exit");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Write("Enter amount you want to withdraw: ");
+                        var amountWithdraw = Decimal.Parse(Console.ReadLine());
+                        accountDefault.WithdrawMoney(amountWithdraw);
+
+                        break;
+                    case "2":
+                        Console.Write("Enter amount you want to deposit: ");
+                        var amountDeposit = Decimal.Parse(Console.ReadLine());
+                        accountDefault.DepositMoney(amountDeposit);
+
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        Console.WriteLine("Enter valid option!");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception information: {0}", e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
