9fd832528096637d173be1392096101a4ffe3f99
efess29 <efess29@gmail.com>
2022-03-26 23:27:59 +0300

    add task 1

---
+++ Lesson-4/Lesson4_Task1/Building.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_Task1
+{
+    public class Building
+    {
+        private int _id;
+        private int _height;
+        private int _floors;
+        private int _flats;
+        private int _entrances;
+        private BuildingType _type;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Building"/>
+        /// </summary>
+        public Building()
+        {
+            LastID = UpdateLastID(LastID);
+            
+            this.Id = LastID;
+            this.Height = _height;
+            this.Floors = _floors;
+            this.Flats = _flats;
+            this.Entrances = _entrances;
+            this.Type = _type;
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Building"/>
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="floors"></param>
+        /// <param name="flats"></param>
+        /// <param name="entrances"></param>
+        /// <param name="type"></param>
+        public Building(int height, int floors, int flats, int entrances, BuildingType type)
+        {
+            LastID = UpdateLastID(LastID);
+
+            this.Id = LastID;
+            this.Height = height;
+            this.Floors = floors;
+            this.Flats = flats;
+            this.Entrances = entrances;
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// Получает или задает ИД здания
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Получает или задает последний использованный ИД здания
+        /// </summary>
+        public static int LastID { get; set; } = 0;
+
+        /// <summary>
+        /// Получает или задает высоту здания
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Получает или задает кол-во этажей здания
+        /// </summary>
+        public int Floors { get; set; }
+        
+        /// <summary>
+        /// Получает или задает кол-во квартир здания
+        /// </summary>
+        public int Flats { get; set; }
+
+        /// <summary>
+        /// Получает или задает кол-во подъездов здания
+        /// </summary>
+        public int Entrances { get; set; }
+
+        /// <summary>
+        /// Получает или задает тип здания
+        /// </summary>
+        public BuildingType Type { get; set; }
+
+        /// <summary>
+        /// Представляет метод вычисления основных характеристик здания
+        /// </summary>
+        /// <param name="building"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static int CalculateCharacteristics(Building building, BuildingCharacteristic param)
+        {
+            if (building == null)
+            {
+                throw new Exception("Building instance is null or empty!");
+            }
+
+            var buildingType = building.Type.ToString();
+            var paramType = param.ToString();
+            int result;
+
+            switch (paramType)
+            {
+                case "FloorHeight":
+                    result = CalculateFloorHeight(buildingType);
+                    return result;
+                case "FlatsPerEntrance":
+                    result = CalculateFlatsPerFloor(buildingType);
+                    return result;
+                case "FlatsPerFloor":
+                    result = CalculateFlatsPerEntrance(buildingType);
+                    return result;
+                default:
+                    break;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Представляет метод вычисления высоты этажа
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static int CalculateFloorHeight(string type)
+        {
+            switch (type)
+            {
+                case "A":
+                    return 4;
+                case "B":
+                    return 2;
+                case "C":
+                    return 3;
+                default:
+                    break;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Представляет метод вычисления кол-ва квартир на этаже
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static int CalculateFlatsPerFloor(string type)
+        {
+            switch (type)
+            {
+                case "A":
+                    return 5;
+                case "B":
+                    return 6;
+                case "C":
+                    return 4;
+                default:
+                    break;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Представляет метод вычисления кол-ва квартир в подъезде
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static int CalculateFlatsPerEntrance(string type)
+        {
+            switch (type)
+            {
+                case "A":
+                    return 20;
+                case "B":
+                    return 45;
+                case "C":
+                    return 19;
+                default:
+                    break;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Представляет метод вычисления последнего ИД здания
+        /// </summary>
+        /// <param name="lastID"></param>
+        /// <returns></returns>
+        public int UpdateLastID(int lastID)
+        {
+            return ++lastID;
+        }
+
+        /// <summary>
+        /// Представляет перечисление типов зданий
+        /// 1 - сталинка, 2 - хрущевка, 3 - брежневка
+        /// </summary>
+        public enum BuildingType
+        {
+            A = 1,
+            B = 2,
+            C = 3
+        }
+
+        /// <summary>
+        /// Представляет перечисление вычисляемых характеристик
+        /// 1 - высота этажа, 2 - квартир на подъезд, 3 - квартир на этаж
+        /// </summary>
+        public enum BuildingCharacteristic
+        {
+            FloorHeight = 1,
+            FlatsPerEntrance = 2,
+            FlatsPerFloor = 3
+        }
+    }
+}
---
+++ Lesson-4/Lesson4_Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using static Lesson4_Task1.Building;
+
+namespace Lesson4_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buildings = new List<Building>()
+            {
+                new Building(150, 10, 60, 3, (BuildingType)2),
+                new Building(50, 5, 26, 1, (BuildingType)1),
+                new Building(62, 8, 45, 2, (BuildingType)3)
+            };
+
+            foreach (var building in buildings)
+            {
+                int floorHeight = CalculateCharacteristics(building, (BuildingCharacteristic)1);
+                int flatsPerFloor = CalculateCharacteristics(building, (BuildingCharacteristic)2);
+                int flatsPerEntrance = CalculateCharacteristics(building, (BuildingCharacteristic)3);
+
+                Console.WriteLine("=============");
+                Console.WriteLine($"BUILDING {building.Id}");
+                Console.WriteLine("-------------");
+                Console.WriteLine($"ID: {building.Id}");
+                Console.WriteLine($"Type: {building.Type}");
+                Console.WriteLine($"Floors: {building.Floors}");
+                Console.WriteLine($"Flats: {building.Flats}");
+                Console.WriteLine($"Entrances: {building.Entrances}");
+                Console.WriteLine($"Floor height: {floorHeight}");
+                Console.WriteLine($"Flats per floor: {flatsPerFloor}");
+                Console.WriteLine($"Flats per entrance: {flatsPerEntrance}");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
