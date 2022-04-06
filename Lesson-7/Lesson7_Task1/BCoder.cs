2ada4d93152c62cf893bf3ee02cddbce56859657
efess29 <efess29@gmail.com>
2022-04-06 22:13:38 +0300

    add task 1

---
+++ Lesson-7/Lesson7_Task1/BCoder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_Task1
+{
+    public sealed class BCoder : ICoder
+    {
+        /// <summary>
+        /// Реализует метод дешифрования
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string Decode(string s)
+        {
+            return Encode(s);
+        }
+
+        /// <summary>
+        /// Реализует метод шифрования
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string Encode(string s)
+        {
+            if (s is null)
+            {
+                throw new ArgumentException($"{nameof(s)} is Null");
+            }
+
+            char[] arr = new char[s.Length];
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                int firstRusLow = 1072;
+                int lastRusLow = 1103;
+
+                int firstEngLow = 97;
+                int lastEngLow = 122;
+
+                var isLow = char.IsLower(s[i]);
+                var sCharInt = (int)char.ToLower(s[i]);
+                
+                if (sCharInt >= firstRusLow && sCharInt <= lastRusLow)
+                {
+                    sCharInt = lastRusLow - (sCharInt - firstRusLow);
+                }
+                
+                if (sCharInt >= firstEngLow && sCharInt <= lastEngLow)
+                {
+                    sCharInt = lastEngLow - (sCharInt - firstEngLow);
+                }
+
+                if (isLow)
+                {
+                    arr[i] = (char)sCharInt;
+                }
+
+                else
+                {
+                    arr[i] = char.ToUpper((char)sCharInt);
+                }
+            }
+
+            return new string(arr);
+        }
+    }
+}
---
+++ Lesson-7/Lesson7_Task1/ICoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_Task1
+{
+    public interface ICoder
+    {
+        /// <summary>
+        /// Представляет метод шифрования
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        string Encode(string s);
+
+        /// <summary>
+        /// Представляет метод дешифрования
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        string Decode(string s);
+    }
+}
